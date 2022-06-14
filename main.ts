b7a06ddeef8bcc91d94f9ec06ce131551df9cfea
Nexeno <34087438+Nexeno@users.noreply.github.com>
2022-06-14 13:14:50 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     if (padle_location == 0) {
-        padle_1.change(LedSpriteProperty.X, -1)
-        padle_2.change(LedSpriteProperty.X, -1)
+        padle1.change(LedSpriteProperty.X, -1)
+        padle2.change(LedSpriteProperty.X, -1)
         padle_location += -1
     } else if (padle_location == 1) {
-        padle_1.change(LedSpriteProperty.X, -1)
-        padle_2.change(LedSpriteProperty.X, -1)
+        padle1.change(LedSpriteProperty.X, -1)
+        padle2.change(LedSpriteProperty.X, -1)
         padle_location += -1
     } else if (padle_location == 2) {
-        padle_1.change(LedSpriteProperty.X, -1)
-        padle_2.change(LedSpriteProperty.X, -1)
+        padle1.change(LedSpriteProperty.X, -1)
+        padle2.change(LedSpriteProperty.X, -1)
         padle_location += -1
     } else {
     	
@@ -17,24 +17,24 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.B, function () {
     if (padle_location == 0) {
-        padle_1.change(LedSpriteProperty.X, 1)
-        padle_2.change(LedSpriteProperty.X, 1)
+        padle1.change(LedSpriteProperty.X, 1)
+        padle2.change(LedSpriteProperty.X, 1)
         padle_location += 1
     } else if (padle_location == -1) {
-        padle_1.change(LedSpriteProperty.X, 1)
-        padle_2.change(LedSpriteProperty.X, 1)
+        padle1.change(LedSpriteProperty.X, 1)
+        padle2.change(LedSpriteProperty.X, 1)
         padle_location += 1
     } else if (padle_location == 1) {
-        padle_1.change(LedSpriteProperty.X, 1)
-        padle_2.change(LedSpriteProperty.X, 1)
+        padle1.change(LedSpriteProperty.X, 1)
+        padle2.change(LedSpriteProperty.X, 1)
         padle_location += 1
     } else {
     	
     }
 })
 let padle_location = 0
-let padle_2: game.LedSprite = null
-let padle_1: game.LedSprite = null
+let padle2: game.LedSprite = null
+let padle1: game.LedSprite = null
 let win_state = 0
 let ball = game.createSprite(2, 2)
 let wall1 = game.createSprite(0, 0)
@@ -42,8 +42,8 @@ let wall2 = game.createSprite(1, 0)
 let wall3 = game.createSprite(2, 0)
 let wall4 = game.createSprite(3, 0)
 let wall5 = game.createSprite(4, 0)
-padle_1 = game.createSprite(1, 4)
-padle_2 = game.createSprite(2, 4)
+padle1 = game.createSprite(1, 4)
+padle2 = game.createSprite(2, 4)
 basic.forever(function () {
     if (ball.isTouching(wall1)) {
         wall1.delete()
@@ -70,12 +70,14 @@ basic.forever(function () {
     }
     if (win_state == 5) {
         basic.showIcon(IconNames.Yes)
+        game.pause()
     }
-    if (ball.isTouching(padle_1)) {
+    if (ball.isTouching(padle1)) {
         basic.showIcon(IconNames.No)
         game.pause()
-    } else if (ball.isTouching(padle_2)) {
+    } else if (ball.isTouching(padle2)) {
         basic.showIcon(IconNames.No)
+        game.pause()
     } else {
     	
     }
